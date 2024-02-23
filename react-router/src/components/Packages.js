f0dbad33e31620a4a807e699471a5940952afe69
CoderKaiHan <hankaicoder@gmail.com>
2024-02-22 20:40:55 -0500

    Implement React Routes

--- react-router/src/components/Packages.js
+++ react-router/src/components/Packages.js
@@ -1,12 +1,21 @@
 
+import Home from "./Home";
 
-export default function Packages() {
+export default function Packages(props) {
+
+    const packages = props.packages;
 
     return (
         <div>
+            {<Home/>}
             <div className="packages">
                 <div className="packagesHeader">
                     <h1>Our Packages</h1>
+                    {packages.map((packageItem)=>{
+                        return(
+                            <h2>{packageItem}</h2>
+                        )
+                    })}
                 </div>
                 
             </div>
